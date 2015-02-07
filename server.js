4b9779b08ab8bfe7e5c370f277fe17cef6d5c2fa
jarl-alejandro <jarlalejor@gmail.com>
2015-02-06 19:35:07 -0500

    OK esta si

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ this.express = new Express();
 
 var server = http.createServer(this.express.app);
 
-server.listen(3000, function(){
-	console.log("Server running in http://localhost:3000/");
+server.listen(this.express.get('port'), function(){
+	console.log("Server running in http://localhost:8000/");
 });
